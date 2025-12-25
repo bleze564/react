b18c97f9c367537677133d5349733cc531a16dd6
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-12-25 20:21:35 +0200

    31

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import { AiFillGithub } from "react-icons/ai"
+
 import "./App.css";
-// import { Name } from './components/name';
-// import { Obj } from './components/obj';
-// import { PaintingList } from './components/paintingList';
-// import paintingData from './painting.json'
 import { Status } from "./components/Status";
 import { UserCard } from "./components/UserCard";
 const user1 = {
@@ -12,7 +8,6 @@ const user1 = {
   city: "Львів",
   status:true
 };
-const products = ["Хліб", "Молоко", "Сир", "Яблука"];
 const App = () => {
   return (
     <main className="main">
@@ -24,12 +19,13 @@ const App = () => {
     </main>
   );
 };
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "grey", color: "#607D8B" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
+
 export default App;
-const colorPickerOptions = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "grey", color: "#607D8B" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F51B5" },
-];
